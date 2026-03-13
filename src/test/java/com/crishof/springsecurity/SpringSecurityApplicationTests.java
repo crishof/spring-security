8bec192e85f9efe8957d874a3ee935e16e36e7c5
Cristian Hoffmann <cristianhof@gmail.com>
2026-03-13 14:17:25 -0300

    add profiles

--- src/test/java/com/crishof/springsecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/crishof/springsecurity/SpringSecurityApplicationTests.java
@@ -2,8 +2,10 @@ package com.crishof.springsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringSecurityApplicationTests {
 
     @Test
